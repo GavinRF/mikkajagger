3ba1f75726cf85b9554c506744ea3ced05ff078d
GavFior <gavin@woodsofplenty.com>
2020-06-17 16:38:53 -0700

    preloader 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 
 window.addEventListener('load', function(){
     var overlay = document.getElementById("overlay");
-    var loader = document.getElementById("preloader");
  
      overlay.style.height = '0%';
      overlay.style.opacity = '.0';
@@ -9,6 +8,6 @@ window.addEventListener('load', function(){
      overlay.style.transitionTimingFunction = 'ease-in-out';
      overlay.style.transition = 'all .8s'; 
  
-    $("#preloader").fadeOut(4000); 
+    // $("#preloader").fadeOut(4000); 
  
  });
